9f54a9582ae3c907cbe3ff39ac5f21dc91bec9cb
Georgiy Kulak <kulakgeorgij@gmail.com>
2020-07-25 00:31:05 +0300

    Add greeting in the start of main()

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main( int argc, char** argv )
 {
+    std::cout << "Welcome to ImagineDB!" << std::endl;
+
     auto exitCode = im::process( argc, argv );
 
     return static_cast< int >( exitCode );
